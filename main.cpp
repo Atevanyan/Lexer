ef71b3fdb2f8aa8ac5b2b20bbb35214b70ed61cd
eugenelee123 <eugene097@csu.fullerton.edu>
2019-02-19 19:27:30 -0800

    Created fileread() function
    took logic out of main and put it in a function, added addition comments for clarity, and removed unnecessary variables and headers

--- main.cpp
+++ main.cpp
@@ -1,45 +1,52 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <list>
+#include <string>
 
 
 using namespace std;
+void fileread(string filename);
 
 int main(int argc, const char * argv[]) {
     
-    char read;
-    string lex;
-    list<char>mylist;
-    
-    ifstream infile("/Users/eugenelee/Documents/School Stuff/CPSC 323/sample input file for assignment 1.txt");
-    
-    if(infile.is_open()){
-        while(!infile.eof()){
-            infile>>read;
-            mylist.push_back(read);
-            }
-        }
-    
-    //Remove comments
-    for(list<char>::iterator it=mylist.begin();it!=mylist.end();++it){
-        if(*it=='!'){
-            do{
-                mylist.erase(it);
-                it++;
-            } while(*it!='!');
-        }
+    fileread("/Users/eugenelee/Documents/School Stuff/CPSC 323/sample input file for assignment 1.txt");
+    return 0;
+}
+
+//Input file stream that will read file
+void fileread(string filename){
+//Characters that I will insert into list
+char read;
+//List of the characters
+list<char>mylist;
+
+ifstream infile(filename);
+
+if(infile.is_open()){
+    while(!infile.eof()){
+        infile>>read;
+        mylist.push_back(read);
     }
-    //Pops remaining exclamation point off of stack
-    mylist.pop_front();
+}
+
+//Remove comments
+for(list<char>::iterator it=mylist.begin();it!=mylist.end();++it){
+    if(*it=='!'){
+        do{
+            mylist.erase(it);
+            it++;
+        } while(*it!='!');
+    }
+}
     
-    //Output file without comments
-    for(list<char>::iterator it=mylist.begin();it!=mylist.end();++it){
-        cout<<*it<<" ";
+//Pops remaining exclamation point off of stack
+mylist.pop_front();
+
+//Output file without comments
+for(list<char>::iterator it=mylist.begin();it!=mylist.end();++it){
+    cout<<*it<<" ";
     }
     
     cout<<"\ndone"<<"/t";
-    return 0;
 }
