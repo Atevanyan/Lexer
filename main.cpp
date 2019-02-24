06cdc77bee9b45a9b7b8fdac16be77d6e2b5848f
Andrew Nomura <drew97.nomura@gmail.com>
2019-02-24 11:34:15 -0800

    added more token arrays and more helper functions

--- main.cpp
+++ main.cpp
@@ -8,13 +8,24 @@
 
 using namespace std;
 
-const string keywords[] = {
-	"if", "ifend", "put", "get", "else",
-	"while", "whileend", "return", "true", "false",
-	"function", "int", "boolean", "real", "string", "do", "char"};
+const string keywords[] = {"asm", "else", "new", "this", "auto", "enum", "operator", "throw", "bool", 
+"explicit", "private", "true", "break", "export", "protected", "try", "case", "extern", "public", 
+"typedef", "catch", "false", "register", "typeid", "char", "float", "reinterpret_cast", "typename", 
+"class", "for", "return", "union", "const", "friend", "short", "unsigned", "const_cast", "goto", "signed", 
+"using", "continue", "if", "sizeof",    "virtual", "default", "inline", "static", "void", "delete", "int", 
+"static_cast", "volatile", "do", "long", "struct", "wchar_t", "double", "mutable", "switch", "while", 
+"dynamic_cast", "namespace", "template",};
 
 const string operators[] = {
 	"=", "==", "+", "-", "*", "/", ">", "<", ">=", "<=", "|", "&&" };
+
+string constants[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+
+string separators[] = { "[", "]", "(", ")", "{", "}", ",", ";", ":", ".", "#"};
+
+string identifiers[] = { "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
 bool isKeyword(string input)
 {
@@ -31,6 +42,36 @@ bool isOperator(string input)
 	for (int i = 0; i < operators->size(); i++)
 	{
 		if (input == operators[i])
+			return true;
+	}
+	return false;
+}
+
+bool isConstant(string input)
+{
+	for (int i = 0; i < constants->size(); i++)
+	{
+		if (input == constants[i])
+			return true;
+	}
+	return false;
+}
+
+bool isSeparator(string input)
+{
+	if (int i = 0; i < separators->size(); i++)
+	{
+		if (input == separators[i])
+			return true;
+	}
+	return false;
+}
+
+bool isIdentifier(string input)
+{
+	for (int i = 0; i < identifiers->size(); i++)
+	{
+		if (input == identifier[i])
 			return true;
 	}
 	return false;
