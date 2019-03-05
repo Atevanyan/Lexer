858a5cf4fcfea1591a0e9b98f8b90c4988572052
Atevanyan <32005247+Atevanyan@users.noreply.github.com>
2019-03-05 15:40:45 -0800

    Now outputting each lexeme
    still need to work out parsing the separator when there is no space

--- main.cpp
+++ main.cpp
@@ -18,17 +18,11 @@ const string keywords[] = { "asm", "else", "new", "this", "auto", "enum", "opera
 
 const string operators[] = { "=", "==", "+", "-", "*", "/", ">", "<", ">=", "<=", "|", "&&" };
 
-string constants[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-
 string separators[] = { "[", "]", "(", ")", "{", "}", ",", ";", ":", ".", "#" };
-
-string identifiers[] = { "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
 bool isKeyword(string input)
 {
-	for (int i = 0; i < keywords->size(); i++)
+	for (int i = 0; i < (sizeof(keywords) / sizeof(*keywords)); i++)
 	{
 		if (input == keywords[i])
 			return true;
@@ -38,7 +32,7 @@ bool isKeyword(string input)
 
 bool isOperator(string input)
 {
-	for (int i = 0; i < operators->size(); i++)
+	for (int i = 0; i < (sizeof(operators) / sizeof(*operators)); i++)
 	{
 		if (input == operators[i])
 			return true;
@@ -46,19 +40,10 @@ bool isOperator(string input)
 	return false;
 }
 
-bool isConstant(string input)
-{
-	for (int i = 0; i < constants->size(); i++)
-	{
-		if (input == constants[i])
-			return true;
-	}
-	return false;
-}
 
 bool isSeparator(string input)
 {
-	for (int i = 0; i < separators->size(); i++)
+	for (int i = 0; i < (sizeof(separators) / sizeof(*separators)); i++)
 	{
 		if (input == separators[i])
 			return true;
@@ -66,15 +51,6 @@ bool isSeparator(string input)
 	return false;
 }
 
-bool isIdentifier(string input)
-{
-	for (int i = 0; i < identifiers->size(); i++)
-	{
-		if (input == identifiers[i])
-			return true;
-	}
-	return false;
-}
 
 int main() {
 
@@ -84,9 +60,9 @@ int main() {
 
 	ifstream infile("input.txt");
 	infile >> noskipws;
-	if (infile.is_open()) 
+	if (infile.is_open())
 	{
-		while (!infile.eof()) 
+		while (!infile.eof())
 		{
 			infile >> read;
 			mylist.push_back(read);
@@ -94,14 +70,14 @@ int main() {
 	}
 
 	//Remove comments
-	for (list<char>::const_iterator it = mylist.begin(); it != mylist.end(); ++it) 
+	for (list<char>::const_iterator it = mylist.begin(); it != mylist.end(); ++it)
 	{
 		if (*it == '!') {
-			do 
+			do
 			{
-				//it = mylist.erase(it);
-				mylist.pop_back();
+				list<char>::const_iterator temp = it;
 				*it++;
+				mylist.erase(temp);
 			} while (*it != '!');
 			it = mylist.erase(it);
 		}
@@ -113,14 +89,44 @@ int main() {
 	ofstream somefile;
 
 	somefile.open("NoComments.txt");
-	for (list<char>::iterator it = mylist.begin(); it != mylist.end(); ++it) 
+	for (list<char>::iterator it = mylist.begin(); it != mylist.end(); ++it)
 	{
 		cout << *it;
 		somefile << *it;
 	}
+	
+	somefile.close();
 
+	string temp;
+	//read in from new txt file with no comments
+	ifstream newFile("NoComments.txt");
 
-	cout << "\ndone" << "/t";
+	if (newFile.is_open())
+	{
+		cout << "Keyword                       Lexemes" << endl << endl;
+		//until we reach the end of the txt doc get one line at a time
+		while (newFile >> temp)
+		{
+
+			if (isKeyword(temp))
+			{
+				cout << "Keyword              =            " << temp << endl;
+			}
+			else if (isSeparator(temp))
+			{
+				cout << "Seperator            =            " << temp << endl;
+			}
+			else if(isOperator(temp))
+			{
+				cout << "Operator             =            " << temp << endl;
+			}
+			//if not any of the above then assume it is identifier
+			else
+			{
+				cout << "Identifier           =            " << temp << endl;
+			}
+		}
+	}
 
 	system("PAUSE");
 	return 0;
